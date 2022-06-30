1c823cbc6237c230fc27360e9d1d9defee259492
AleksanyanNarek <87985008+AleksanyanNarek@users.noreply.github.com>
2022-06-30 12:06:33 +0400

    Name change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import navBack from '../images/navBack.jpg';
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav className={`${libraryStatus ? "active-library-nav" : ""}`} style={{ backgroundImage: `url(${navBack})` }}>
-      <h2>Armen`s playlist</h2>
+      <h2>Narek`s playlist</h2>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library
         <FontAwesomeIcon icon={faMusic}/>
